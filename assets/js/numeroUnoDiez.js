03c159d041dcaf28f2fafe5be2df4ca1a9706ff2
Quiroga J <juanqi3489@gmail.com>
2024-05-13 11:14:19 -0300

    Programa  que itera del 0 a 1 y que describe si son par o impar FINALIZADO

---
+++ assets/js/numeroUnoDiez.js
@@ -0,0 +1,10 @@
+let contador = -1;
+
+while (contador < 10){
+    contador ++;
+    if (contador % 2 === 0){
+        console.log(contador + " Es par");
+    } else{
+        console.log(contador + " Es impar");
+    }
+}
